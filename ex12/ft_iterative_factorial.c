af73d6c0cb3e10769370b098d13061c88b34d69d
42 Adel FC <adel.fodilcherif@gmail.com>
2024-11-07 13:14:29 +0100

    ADD ex 12-15

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,21 +6,42 @@
 /*   By: afodil-c <afodil-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 09:31:05 by afodil-c          #+#    #+#             */
-/*   Updated: 2024/11/06 13:44:58 by afodil-c         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:49:21 by afodil-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
-    
+	int	i;
+	int	result;
+
+	i = nb - 1;
+	result = nb;
+	if (nb < 0)
+	{
+		return (0);
+	}
+	else if (nb == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		while (i > 0)
+		{
+			result *= i;
+			i--;
+		}
+		return (result);
+	}
 }
 
-// #include <stdio.h>
+#include <stdio.h>
 
-// int	main(void)
-// {
-// 	printf("Pour -2, c'est : %d\n", ft_iterative_factorial(-2));
-// 	printf("Pour 0, c'est : %d\n", ft_iterative_factorial(0));
-// 	printf("Pour 2, c'est : %d\n", ft_iterative_factorial(2));
-// 	printf("Pour 10, c'est : %d\n", ft_iterative_factorial(70));
-// }
+int	main(void)
+{
+	printf("Pour -2, c'est : %d\n", ft_iterative_factorial(-2));
+	printf("Pour 0, c'est : %d\n", ft_iterative_factorial(0));
+	printf("Pour 2, c'est : %d\n", ft_iterative_factorial(2));
+	printf("Pour 10, c'est : %d\n", ft_iterative_factorial(10));
+}
--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,37 @@
 /*   By: afodil-c <afodil-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 10:50:37 by afodil-c          #+#    #+#             */
-/*   Updated: 2024/11/06 10:50:43 by afodil-c         ###   ########.fr       */
+/*   Updated: 2024/11/07 11:51:02 by afodil-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_recursive_factorial(int nb)
+{
+	int	i;
+	int	result;
+
+	i = nb - 1;
+	result = nb;
+	if (nb < 0)
+	{
+		return (0);
+	}
+	else if (nb == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (result *= ft_recursive_factorial(nb-1));
+	}
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Pour -2, c'est : %d\n", ft_recursive_factorial(-2));
+	printf("Pour 0, c'est : %d\n", ft_recursive_factorial(0));
+	printf("Pour 2, c'est : %d\n", ft_recursive_factorial(2));
+	printf("Pour 10, c'est : %d\n", ft_recursive_factorial(10));
+}
